b101812c0c43541a5ecee32779ac8bfd8d95622e
Зазунов Денис <99178404+DenisZaz@users.noreply.github.com>
2022-04-03 18:47:23 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,89 @@
 #include <map>
 #include "tstack.h"
 
-std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+using namespace std;
+
+int priority(char op) {
+  switch (op) {
+    case '(': return 0;
+    case ')': return 1;
+    case '+': return 2;
+    case '-': return 2;
+    case '*': return 3;
+    case '/': return 3;
+    default: return 4;
+  }
 }
 
-int eval(std::string pref) {
-  // добавьте код
+int calc(char op, int x, int y) {
+  switch (op) {
+    case '+': return x + y;
+    case '-': return x - y;
+    case '*': return x * y;
+    case '/': return x / y;
+  }
   return 0;
+}
+
+std::string infx2pstfx(std::string inf) {
+TStack <char, 100> charstack;
+string str = "";
+for (int i = 0; i < inf.length(); i++) {
+  if (priority(inf[i]) == 4) {
+    str += inf[i];
+  } else if (priority(inf[i]) > 1 && priority(inf[i]) < 4) {
+      if (charstack.isEmpty() || priority(charstack.get()) == 0) {
+        charstack.push(inf[i]);
+          str += " ";
+         } else if (priority(inf[i]) > priority(charstack.get())) {
+             charstack.push(inf[i]);
+             str += " ";
+         } else if (priority(inf[i]) <= priority(charstack.get())) {
+             str += " ";
+             str += charstack.get();
+             charstack.pop();
+             str += " "; 
+             while ((priority(inf[i]) <= priority(charstack.get()) || priority(charstack.get()) != 0) && !charstack.isEmpty()) {
+               str += " ";
+               str += charstack.get();
+               charstack.pop();
+               str += " ";
+             }
+             charstack.push(inf[i]);
+          }
+        } else if (priority(inf[i]) == 0) {
+            charstack.push(inf[i]);
+        } else if (priority(inf[i]) == 1) {
+            while (priority(charstack.get()) != 0) {
+              str += " ";
+              str += charstack.get();
+              charstack.pop();
+            }
+            charstack.pop();
+        } 
+    }
+    while (!charstack.isEmpty()) {
+      str += " ";
+      str += charstack.get();
+      charstack.pop();
+    }
+  return str;
+}
+
+int eval(std::string postf) {
+TStack <int, 100> numstack;
+int sum = 0, x = 0, y = 0;
+for (int i = 0; i < postf.size(); i+=2) {
+  if (priority(postf[i]) == 4) {
+    numstack.push(atoi(&postf[i]));
+  } else {
+      y = numstack.get();
+      numstack.pop();
+      x = numstack.get();
+      numstack.pop();
+      sum = calc(postf[i], x, y);
+      numstack.push(sum);
+    }
+}
+return sum;
 }
